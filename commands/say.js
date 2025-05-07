fbd63eec6b5a5c6ae1b0e391d30c7dcaab86bcbd
moozxdz <76737096+maazinalthaf@users.noreply.github.com>
2025-05-07 08:08:35 +0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,7 @@ const {EmbedBuilder, PermissionsBitField} = require('discord.js');
 module.exports = {
     name: 'say',
     async execute(client, message, args) {
-// Check if the user has permission to use the say command
+
 if (!message.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
     const embed = new EmbedBuilder()
     .setColor('#C83636')
@@ -11,10 +11,10 @@ if (!message.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
     return message.reply({ embeds: [embed], allowedMentions: {repliedUser: false} });
   }
 
-  // Get the text to be said
+  
   const text = args.join(' ');
 
-  // Check if no message is provided
+  
   if (!text) {
     const embed = new EmbedBuilder()
     .setColor('#FFCC32')
@@ -22,10 +22,10 @@ if (!message.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
     return message.reply({ embeds: [embed], allowedMentions: {repliedUser: false} });
   }
 
-  // Delete the message that triggered the command
+  
   message.delete().catch(console.error);
 
-  // Send the text back to the channel
+  
   message.channel.send(text);
     }
 }
