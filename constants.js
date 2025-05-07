8f143a3a6568d4c3e05746fae30faf4d72d2e4ca
maazinalthaf@gmail.com <maazinalthaf@gmail.com>
2025-05-07 11:27:23 +0300

    constants added

---
+++ constants.js
@@ -0,0 +1,14 @@
+const prefix = '.';
+const embed_color = '#5865F2';
+const emojis = {
+  tick: '<:success:1284139732158251078>',
+  error: '<:error:1332418281675558963>',
+  cross: '<:cross:1332418251849732206>',
+  reaction: '<:roles:1332417540810342532>',
+};
+
+module.exports = {
+ prefix,
+ embed_color,
+ emojis
+};
