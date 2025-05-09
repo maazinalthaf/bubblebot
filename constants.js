204940034d272915f44b3528b6fb7c89355ad084
moozxdz <76737096+maazinalthaf@users.noreply.github.com>
2025-05-09 13:04:51 +0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 const prefix = '.';
 const embed_color = '#78807a';
 const emojis = {
-  tick: '<:success:1284139732158251078>',
-  error: '<:error:1284139732158251078>',
-  cross: '<:cross:1284139732158251078>',
+  tick: '<:tick:1332418339372273684>',
+  error: '<:error:1332418281675558963>',
+  cross: '<:cross:1332418251849732206>',
   reaction: '<:roles:1332417540810342532>',
   bell: '🔔',
   info: 'ℹ️',
