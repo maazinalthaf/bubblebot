9b9bfdd215d934faea5b812074e6ff3b2e026565
moozxdz <76737096+maazinalthaf@users.noreply.github.com>
2025-07-08 02:25:08 +0300

    Update clear.js

--- commands/Moderation/clear.js
+++ commands/Moderation/clear.js
@@ -1,26 +1,36 @@
 const { EmbedBuilder, PermissionsBitField } = require('discord.js');
-const {embed_color, emojis, prefix } = require('../../constants');
+const { embed_color, emojis } = require('../../constants');
 
 module.exports = {
     name: 'purge',
     aliases: ['clear', 'c'],
+    description: 'Deletes a specified number of messages (1-100) from the channel',
+    usage: '<number>',
     async execute(client, message, args) {
         if (!message.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
             const embed = new EmbedBuilder()
                 .setColor('#C83636')
-                .setDescription(`${emoji.cross} You do not have permission to use this command.`);
+                .setDescription(`${emojis.cross} You do not have permission to use this command.`);
             return message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
         }
 
-        
+        // Delete the command message
         await message.delete().catch(() => {});
+
+        // Validate input
+        if (!args[0]) {
+            const embed = new EmbedBuilder()
+                .setColor('#FFCC32')
+                .setDescription(`${emojis.error} Please specify the number of messages to delete.\nUsage: \`${this.name} ${this.usage}\``);
+            return message.channel.send({ embeds: [embed] }).then(msg => setTimeout(() => msg.delete(), 5000));
+        }
 
         const amount = parseInt(args[0]);
 
         if (isNaN(amount)) {
             const embed = new EmbedBuilder()
                 .setColor('#FFCC32')
-                .setDescription(`${emojis.error} Please provide a valid number.`);
+                .setDescription(`${emojis.error} Please provide a valid number.\nUsage: \`${this.name} ${this.usage}\``);
             return message.channel.send({ embeds: [embed] }).then(msg => setTimeout(() => msg.delete(), 5000));
         }
 
@@ -32,23 +42,24 @@ module.exports = {
         }
 
         try {
-            // Fetch messages (including the command message)
-            const messages = await message.channel.messages.fetch({ limit: amount + 1 });
+            
+            const messages = await message.channel.messages.fetch({ limit: amount });
             
             // Delete all fetched messages
             const deletedMessages = await message.channel.bulkDelete(messages, true);
             
+            // Send confirmation
             const embed = new EmbedBuilder()
                 .setColor('#77B255')
-                .setDescription(`${emojis.tick} Successfully deleted ${deletedMessages.size - 1} messages.`);
+                .setDescription(`${emojis.tick} Successfully deleted ${deletedMessages.size} messages.`);
                 
             const reply = await message.channel.send({ embeds: [embed] });
-            setTimeout(() => reply.delete().catch(() => {}), 2000);
+            setTimeout(() => reply.delete().catch(() => {}), 3000);
         } catch (error) {
             console.error('Error deleting messages:', error);
             const embed = new EmbedBuilder()
                 .setColor('#FFCC32')
-                .setDescription(`${emojis.error} An error occurred while deleting messages. Make sure the messages are not older than 14 days.`);
+                .setDescription(`${emojis.error} An error occurred while deleting messages. ${error.message.includes('Maximum') ? 'Messages must be younger than 14 days.' : 'Please try again.'}`);
             message.channel.send({ embeds: [embed] }).then(msg => setTimeout(() => msg.delete(), 5000));
         }
     }
